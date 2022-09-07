1b933cbba4993ec2d5852816d8805c5c79112c78
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-09-07 17:10:17 -0300

    H2 to Mysql

--- src/main/java/com/api/phones/controllers/PhoneController.java
+++ src/main/java/com/api/phones/controllers/PhoneController.java
@@ -10,13 +10,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.phones.dto.PhoneDTO;
@@ -26,6 +26,7 @@ import com.api.utils.StringUtils;
 
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class PhoneController {
     
     @Autowired
