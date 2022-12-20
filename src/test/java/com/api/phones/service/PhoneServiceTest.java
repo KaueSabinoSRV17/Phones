9d2a1eaec223fe4d979b7e21e1771d9954806299
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-12-20 23:01:19 +0000

    tests: unhappy path tests

--- src/test/java/com/api/phones/service/PhoneServiceTest.java
+++ src/test/java/com/api/phones/service/PhoneServiceTest.java
@@ -7,18 +7,20 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Pageable;
 
+import com.api.phones.exceptions.DomainException;
 import com.api.phones.model.Phone;
 import com.api.phones.repository.PhoneRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-public class PhoneServiceTest {
+class PhoneServiceTest {
 
     @Mock
     private PhoneRepository repository;
@@ -37,15 +39,15 @@ public class PhoneServiceTest {
         autoClosable.close(); 
     }
 
+    // <=== HAPPY PATH TESTS ===>
+
     @Test
     void canGetAllPhonesTest__ShouldBeSucessfull() {
 
         // Given
         var pageable = Mockito.any(Pageable.class);
-        
         // When
         underTest.findAllPhones(pageable);
-
         // Then
         verify(repository).findAll(pageable);
 
@@ -56,10 +58,8 @@ public class PhoneServiceTest {
 
         // Given
         int id = 1;
-
         // When
         underTest.findPhoneById(id);
-
         // Then
         verify(repository).findById(id);
 
@@ -100,6 +100,86 @@ public class PhoneServiceTest {
         // Then
         verify(repository).existsById(id);
         verify(repository).deleteById(id);
+
+    }
+
+    @Test
+    void canCheckIfAPhoneExistsByName__ShouldBeSucessfull() {
+
+        // Given
+        String name = "A10";
+        // When
+        underTest.existsByName(name);
+        // Then
+        verify(repository).existsByName(name);
+
+    }
+
+    @Test
+    void canCheckIfPhoneExistsById__ShouldBeSucessfull() {
+
+        // Given
+        int id = 1;
+        // When
+        underTest.existsById(id);
+        // Then
+        verify(repository).existsById(id);
+
+    }
+
+    // <=== UNHAPPY PATH TESTS ===>
+
+    @Test
+    void cannotSaveARepeatedPhone__ShouldThrowDomainException() {
+
+        // Given
+        var phone = new Phone(1, "A10", "Samsung");
+        when(repository.existsByName(phone.getName()))
+            .thenReturn(true);
+
+        doThrow(DomainException.class)
+            .when(repository)
+            .save(phone);
+
+        // When
+        assertThrows(DomainException.class, () -> underTest.savePhoneOnDataBase(phone));
+
+        // Then
+        verify(repository, never()).save(phone);
+    }
+
+    @Test
+    void cannotDeleteAPhoneThatDoesNotExists__ShouldThrowDomainException() {
+
+        // Given
+        int id = 1;
+        // When
+        assertThrows(DomainException.class, 
+            () -> underTest.delete(id));
+        // Then
+        verify(repository).existsById(id);
+
+        doThrow(DomainException.class)
+            .when(repository)
+            .deleteById(id);
+
+        verify(repository, never()).deleteById(id);
+
+    }
+
+    @Test
+    void cannotUpdateAPhoneThatDoesNotExists__ShouldThrowDomainException() {
+
+        // Given
+        var phone = new Phone(1, "A10", "Samsung");
+        when(repository.existsById(phone.getId()))
+            .thenReturn(false);
+
+        // When
+        assertThrows(DomainException.class, 
+            () -> underTest.updatePhoneOnDataBase(phone, phone.getId()));
+        // Then
+        verify(repository, never()).save(phone);
 
     }
 
